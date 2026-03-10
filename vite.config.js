4dfb74ffde7a8a073bfbae7e5de61552c1f2a392
YasamSriCharan <2500030149@kluniversity.in>
2026-03-10 18:46:49 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Settings-and-Configuration-Management-UI/',   // 👈 IMPORTANT
+  base: '/Settings-and-Configuration-Management/',   // 👈 IMPORTANT
 })
